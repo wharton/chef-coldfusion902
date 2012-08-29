4d4a61d1694aaf3cbc062520f7bddb8293147fef
Lew Goettner <lew@goettner.net>
2012-08-29 15:18:01 -0400

    Bump version, add depends, fix recipe definition for jws

--- metadata.rb
+++ metadata.rb
@@ -3,16 +3,19 @@ maintainer_email "goettnel@wharton.upenn.edu"
 license          "Apache 2.0"
 description      "Installs/Configures ColdFusion 9.0.2"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 supports 'ubuntu', '= 10.04'
 supports 'ubuntu', '>= 11.04'
+
+depends "apt"
+depends "apache2"
 
 recipe "coldfusion9", "Includes the standalone recipe"
 recipe "coldfusion9::apache", "Configures ColdFusion to run behind the Apache httpd web server"
 recipe "coldfusion9::configure", "Sets ColdFusion configuration settings via the config LWRP"
 recipe "coldfusion9::jvmconfig", "Sets necessary JVM configuration, included by default recipe"
-recipe "coldfusion9::jvmconfig", "Sets webroot for the built in JRun web server (JWS)"
+recipe "coldfusion9::jws", "Sets webroot for the built in JRun web server (JWS)"
 recipe "coldfusion9::standalone", "Installs ColdFusion 9.0.1 in standalone mode, included by default recipe"
 recipe "coldfusion9::trustedcerts", "Imports configured certificates into the JVM truststore"
 
